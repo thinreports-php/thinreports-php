911a7e77602f90c866f569b66c58ceba2345498d
Katsuya HIDAKA <hidakatsuya@users.noreply.github.com>
2015-08-28 00:49:27 +0900

    Revert "Bundle with pre-built unicode fonts for performance improvemnt"

--- test/unit/Thinreports/Generator/PDF/FontTest.php
+++ test/unit/Thinreports/Generator/PDF/FontTest.php
@@ -5,6 +5,11 @@ use Thinreports\TestCase;
 
 class FontTest extends TestCase
 {
+    function setup()
+    {
+        Font::$installed_builtin_fonts = array();
+    }
+
     function test_getFontName()
     {
         $this->assertEquals('Helvetica', Font::getFontName('Helvetica'));
@@ -12,10 +17,35 @@ class FontTest extends TestCase
         $this->assertEquals('Courier', Font::getFontName('Courier New'));
         $this->assertEquals('Times', Font::getFontName('Times New Roman'));
 
+        $this->assertFalse(Font::isInstalledFont('IPAMincho'));
+        $this->assertNotContains('ipam', Font::$installed_builtin_fonts);
+
         $this->assertEquals('ipam', Font::getFontName('IPAMincho'));
-        $this->assertEquals('ipamp', Font::getFontName('IPAPMincho'));
-        $this->assertEquals('ipag', Font::getFontName('IPAGothic'));
-        $this->assertEquals('ipagp', Font::getFontName('IPAPGothic'));
+
+        $this->assertTrue(Font::isInstalledFont('IPAMincho'));
+        $this->assertContains('ipam', Font::$installed_builtin_fonts);
+
+        $this->assertEquals('ipam', Font::getFontName('IPAMincho'));
+    }
+
+    /**
+     * @dataProvider unicodeFontProvider
+     */
+    function test_installBuiltinFont($expected_result, $font_name)
+    {
+        $actual = Font::installBuiltinFont($font_name);
+
+        $this->assertEquals($expected_result, $actual);
+        $this->assertContains($actual, Font::$installed_builtin_fonts);
+    }
+    function unicodeFontProvider()
+    {
+        return array(
+            array('ipam', 'IPAMincho'),
+            array('ipag', 'IPAGothic'),
+            array('ipamp', 'IPAPMincho'),
+            array('ipagp', 'IPAPGothic')
+        );
     }
 
     function test_isBuiltinUnicodeFont()
@@ -23,5 +53,12 @@ class FontTest extends TestCase
         $this->assertFalse(Font::isBuiltinUnicodeFont('unknown font'));
         $this->assertFalse(Font::isBuiltinUnicodeFont('Helvetica'));
         $this->assertTrue(Font::isBuiltinUnicodeFont('IPAGothic'));
+    }
+
+    function test_isInstalledFont()
+    {
+        $this->assertFalse(Font::isInstalledFont('IPAMincho'));
+        Font::$installed_builtin_fonts['IPAMincho'] = 'ipam';
+        $this->assertTrue(Font::isInstalledFont('IPAMincho'));
     }
 }
