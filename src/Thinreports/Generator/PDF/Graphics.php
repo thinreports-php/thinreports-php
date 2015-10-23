1f23ab471b47a6defb04d4c0c0d874e353bcac07
mikami <mikami@tecpresso.co.jp>
2015-10-23 17:36:03 +0900

    Fix Background visibility

--- src/Thinreports/Generator/PDF/Graphics.php
+++ src/Thinreports/Generator/PDF/Graphics.php
@@ -82,20 +82,13 @@ class Graphics
         $style = $this->buildGraphicStyles($attrs);
 
         $drawStyle ="";
-        if(isset($style["stroke"])){
-            $drawStyle.="D";
-        }
-        if(isset($style['fill'])){
-            $drawStyle.="F";
-        }
-
 
         if (empty($attrs['radius'])) {
             $this->pdf->Rect($x, $y, $width, $height,
-                $drawStyle, array('all' => $style['stroke']), $style['fill']);
+                $style['style'], array('all' => $style['stroke']), $style['fill']);
         } else {
             $this->pdf->RoundedRect($x, $y, $width, $height,
-                $attrs['radius'], '1111', null, $style['stroke'], $style['fill']);
+                $attrs['radius'], '1111', $style['style'], $style['stroke'], $style['fill']);
         }
     }
 
@@ -117,7 +110,7 @@ class Graphics
         $style = $this->buildGraphicStyles($attrs);
 
         $this->pdf->Ellipse($cx, $cy, $rx, $ry,
-            0, 0, 360, null, $style['stroke'], $style['fill']);
+            0, 0, 360, $style['style'], $style['stroke'], $style['fill']);
     }
 
     /**
@@ -190,11 +183,12 @@ class Graphics
      */
     public function buildGraphicStyles(array $attrs)
     {
+        $style = "";
         if (empty($attrs['stroke_width'])) {
             $stroke_style = null;
         } else {
             $stroke_color = ColorParser::parse($attrs['stroke_color']);
-
+            $style .= 'D';
             if ($attrs['stroke_dash'] === 'none') {
                 $stroke_dash = null;
             } else {
@@ -209,12 +203,13 @@ class Graphics
         }
 
         if (array_key_exists('fill_color', $attrs) && $attrs['fill_color'] !== 'none') {
+            $style .= 'F';
             $fill_color = ColorParser::parse($attrs['fill_color']);
         } else {
             $fill_color = array();
         }
 
-        return array('stroke' => $stroke_style, 'fill' => $fill_color);
+        return array('stroke' => $stroke_style, 'fill' => $fill_color, 'style' => $style);
     }
 
     /**
